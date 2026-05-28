e6d76afd9f7f6d7a7f22a482a275958e987c6dea
Roman <romalekay0808l@gmail.com>
2026-05-28 22:10:39 +0300

    fix database config

--- gymcrm/settings.py
+++ gymcrm/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'gymcrm.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+    'default': dj_database_url.parse(
+        os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
     )
 }
 
