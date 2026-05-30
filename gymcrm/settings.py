9ea422c1b4139d6fe4e6fbf282fe76fad8aeeaf8
Roman <romalekay0808l@gmail.com>
2026-05-30 10:12:16 +0300

    fix postgres connection

--- gymcrm/settings.py
+++ gymcrm/settings.py
@@ -79,8 +79,9 @@ WSGI_APPLICATION = 'gymcrm.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
